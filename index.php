40cb3d3e0ce265a1c0994621fda2e381036bc852
Gajendra Dubey <gajendra.dubey@codesquaretech.com>
2022-11-04 18:29:16 +0530

    product lineitem

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
+
 <?php
 header("Content-Type:application/json");
 if(isset($_GET['from'])){
 $from = $_GET['from'];
-$response = [
-    "success"=> true,
-    "USD" => "1",
-    "EUR" => 1.02
-];
-echo json_encode($response);
+// $to = $_GET['to'];
+// $amount = $_GET['amount'];
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.apilayer.com/currency_data/live?base=$from",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "apikey: zBxLWPeSDweSSdpyDltGwnJRklLV71wK"
+    ],
+]);
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+echo $response;
 }else{
-    echo "please pass all the perameter";
+	echo "please pass all the perameter";
 }
 ?>
