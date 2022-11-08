04d3c9e4e72a4e49f9c95a7e7d0fc56fb5f0f89e
Gajendra Dubey <gajendra.dubey@codesquaretech.com>
2022-11-08 15:43:33 +0530

    new currency

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 header("Content-Type:application/json");
-if(isset($_GET['from'])){
+if(isset($_GET['from']) && isset($_GET['to']) && isset($_GET['amount']) ){
+    
+$to= $_GET['to'];   
 $from = $_GET['from'];
+$amount = $_GET['amount'];
 $response = [
-    "success"=> true,
-    "USD" => "1",
-    "EUR" => 1.02
+    "new_amount"=> $amount*1.02,
+    "new_currency"=> $to,
+    "old_currency"=> $from,
+    "old_amount"=>$amount
 ];
+
 echo json_encode($response);
+
 }else{
     echo "please pass all the perameter";
 }
